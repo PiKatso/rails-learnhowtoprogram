a563b1c219cc47b12f49ac6a5ff65be5359aa840
Grady Shelton and Kat Gifford <kat@>
2017-06-27 09:01:26 -0700

    we did things

---
+++ db/schema.rb
@@ -0,0 +1,28 @@
+ActiveRecord::Schema.define(version: 20170627155243) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "chapters", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lessons", force: :cascade do |t|
+    t.string "name"
+    t.string "content"
+    t.integer "number"
+    t.integer "section_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sections", force: :cascade do |t|
+    t.string "name"
+    t.integer "chapter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
