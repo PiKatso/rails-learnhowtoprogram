4519829a05b4f7e8699980c91acc38822df863cb
Grady Shelton and Kat Gifford <kat@>
2017-06-27 10:39:01 -0700

    create basic controller functions for lesson crud

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,4 +3,50 @@ class LessonsController < ApplicationController
     @lessons = Lesson.all
   end
 
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def new
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.new
+  end
+
+  def create
+    @section = Section.find(params[:section_id])
+    @lesson = @section.lessons.new(lesson_params)
+    if @lesson.save
+      redirect_to lessons_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @section = Section.find(params[:section_id])
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def update
+    @section = Section.find(params[:section_id])
+    @lesson = Lesson.find(params[:id])
+    if @lesson.update(lesson_params)
+      redirect_to section_path(@lesson.section)
+    else
+      render :edit
+    end
+  end
+
+  def destroy_all
+    @section = Section.find(params[:section_id])
+    @lesson = Lesson.find(params[:id])
+    @lesson.destroy_all
+    redirect_to section_path
+  end
+
+private
+  def lesson_params
+    params.require(:lesson).permit(:name, :content, :number)
+  end
+
 end
